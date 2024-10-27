9af0789e46840052d5ecf895dac2536aab00062c
maulikam <mdave.5191@gmail.com>
2024-10-27 18:08:45 +0530

    first commit

---
+++ src/main/kotlin/com/github/maulikam/springboot_kotlin_tutorial/SpringbootKotlinTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.github.maulikam.springboot_kotlin_tutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringbootKotlinTutorialApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringbootKotlinTutorialApplication>(*args)
+}
